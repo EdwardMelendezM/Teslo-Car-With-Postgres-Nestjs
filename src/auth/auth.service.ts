5e4a1785d0431e1b5090ff35b6430478f207f6ef
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-04-17 09:54:50 -0500

    adding register parcial

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,39 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async register(createUserDto: CreateUserDto) {
+    try {
+      const user = this.userRepository.create(createUserDto);
+      await this.userRepository.save(user);
+      return user;
+    } catch (error: any) {
+      this.handleDbError(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  login() {
+    return 'Este es el login';
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  private handleDbError(error: any): never {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+    console.log(error);
+    throw new InternalServerErrorException('Please check server logs');
   }
 }
---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,31 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity,  PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+
+  @PrimaryGeneratedColumn('uuid')
+  id:string
+
+  @Column('text',{
+    unique:true
+  })
+  email:string
+
+  @Column('text')
+  password:string
+
+  @Column('text')
+  fullName:string
+
+  @Column('bool',{
+    default:true
+  })
+  isActive:boolean
+
+  @Column('text',{
+    array:true, 
+    default:['user']
+  })
+  roles:string[]
+}
