3e1a16ffcdbe9878fdd14063f66528af6e001e2f
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-04-17 10:51:56 -0500

    adding configuration for nest auth and jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,39 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        console.log('JWT Token:', configService.get('JWT_SECRET'));
+
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '2h',
+          },
+        };
+      },
+
+      // JwtModule.register({
+      //   secret: process.env.JWT_SECRET,
+      //   signOptions: {
+      //     expiresIn: '2h',
+      //   },
+      // }),
+    }),
+  ],
   exports: [TypeOrmModule],
 })
 export class AuthModule {}
