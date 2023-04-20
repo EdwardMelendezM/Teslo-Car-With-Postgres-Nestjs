70906cd9cc0a9c0c4ebed091be90b0f70c09ee41
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-04-20 10:19:38 -0500

    adding documentations for dto

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,13 +1,24 @@
 /* eslint-disable prettier/prettier */
 
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator"
 
 export class CreateUserDto{
 
+  @ApiProperty({
+    description:"User email (unique)",
+    default:"myuser@example.com",
+    uniqueItems:true
+
+  })
   @IsString()
   @IsEmail()
   email:string
 
+  @ApiProperty({
+    description:"The password must have a Uppercase, lowercase letter and a number",
+    default:"myuser@example.com",
+  })
   @IsString()
   @MinLength(6)
   @MaxLength(50)
@@ -17,7 +28,10 @@ export class CreateUserDto{
   })
   password: string;
   
-
+  @ApiProperty({
+    description:"The name of the user",
+    default:"Emmael"
+  })
   @IsString()
   @MinLength(1)
   fullName:string
--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,13 +1,23 @@
 /* eslint-disable prettier/prettier */
 
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator"
 
 export class LoginUserDto{
 
+  @ApiProperty({
+    description:"User email",
+    default:"myuser@example.com",
+    uniqueItems:true
+  })
   @IsString()
   @IsEmail()
   email:string
 
+  @ApiProperty({
+    description:"The password of user",
+    default:"*******"
+  })
   @IsString()
   @MinLength(6)
   @MaxLength(50)
